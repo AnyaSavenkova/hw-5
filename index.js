84d6b68ea9a0752a0bb5b098a96418a4226a125b
AnyaSavenkova <annakirry@gmail.com>
2021-12-07 22:59:35 +0200

    modified js file

--- index.js
+++ index.js
@@ -1,20 +1,19 @@
 function enter() {
     let string = '';
     let answer;
-    
-for (let i = 1; ; i++)
-   { 
-    answer = prompt('Enter your word', '');
-        if (answer == null) break;
+
+    for (let i = 1; ; i++) {
+        answer = prompt('Enter your word', '');
+        if (answer === undefined) break;
 
         string = string + answer;
         console.log(string);
 
-            if (i % 3 == 0) {
-            let question = confirm ('Do you want to continue?');
-                if (question == false) break;   
-            } 
-        }       
+        if (i % 3 == 0) {
+            let question = confirm('Do you want to continue?');
+            if (!question) break;
+        }
     }
+}
 
-    enter ();
+enter();
